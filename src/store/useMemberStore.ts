8d38d0293a862a33bafca7ceea831112e6f90a7d
정유경 <loadtume5749@nhndata.com>
2024-09-14 01:59:09 +0900

    📝 Member mock data & store (#77)

---
+++ src/store/useMemberStore.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { create } from 'zustand';
+
+export interface Member {
+  id: number;
+  name: string;
+  email: string;
+  birthDate: string;
+  currentPassword: string;
+  newPassword: null;
+  favorDrinkType: string;
+  role: 'USER' | 'MANAGER';
+  alarmEnabled: boolean;
+  imageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: null;
+}
+
+export interface MemberState {
+  members: Member[];
+  setMembers: (members: Member[]) => void;
+  fetchMembers: () => Promise<void>;
+}
+
+export const useMemberStore = create<MemberState>(set => ({
+  members: [],
+  setMembers: (members: Member[]) => set({ members }),
+  fetchMembers: async () => {
+    try {
+      const response = await axios.get('/member.json');
+      console.log(response.data);
+      set({ members: response.data });
+    } catch (err) {
+      console.log('Error', err);
+    }
+  },
+}));
