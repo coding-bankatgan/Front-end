a6b3239c368479df7466f6bd571bd895d6e8d774
subin114 <bin7851@naver.com>
2024-09-20 18:15:15 +0900

    ✨ Search 각각의 라디오 버튼 클릭 시 Top15 리스트 변경 기능 추가 (#104)

--- src/components/pages/Search/Search.tsx
+++ src/components/pages/Search/Search.tsx
@@ -1,18 +1,47 @@
 import styled from '@emotion/styled';
 import ArrowLeftIcon from './../../../assets/icons/ArrowLeftIcon';
 import { Input } from '@/components/ui/input';
-import { Line } from '../Home/Home';
-import { Badge } from '@/components/ui/badge';
-import handImg from '../../../assets/img/handimg.png';
-import SearchResults from './SearchResults';
 import { useNavigate } from 'react-router-dom';
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import SearchIcon from './../../../assets/icons/SearchIcon';
+import { useEffect, useState } from 'react';
+import { fetchSuggestedDrinksApi, fetchSuggestedTagsApi } from '@/api/postApi';
+
+interface SuggestedTag {
+  tagId: number;
+  tagName: string;
+}
+
+interface SuggestedDrink {
+  drinkId: number;
+  drinkName: string;
+}
+
+type SuggestedData = SuggestedTag | SuggestedDrink;
 
 const Search = () => {
   const navigate = useNavigate();
+  const [searchType, setSearchType] = useState<'tag' | 'drink'>('tag');
+  const [suggestedData, setSuggestedData] = useState<SuggestedData[]>([]); // Top15
 
   const prevBtn = () => {
     navigate(-1);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let data;
+      if (searchType === 'tag') {
+        data = await fetchSuggestedTagsApi();
+      } else if (searchType === 'drink') {
+        data = await fetchSuggestedDrinksApi();
+      }
+      setSuggestedData(data);
+    };
+
+    fetchData();
+  }, [searchType]);
 
   return (
     <SearchLayout>
@@ -24,24 +53,51 @@ const Search = () => {
           검색하기
         </SearchTop>
         <SearchContent>
-          <Input placeholder="#달달한 술" />
-          <span>찾고 싶은 게시글의 태그를 입력해 주세요.</span>
-          <Line />
+          <RadioGroupStyled
+            defaultValue={searchType}
+            onValueChange={value => setSearchType(value as 'tag' | 'drink')}
+          >
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="tag" id="tag" />
+              <Label htmlFor="tag">태그 검색</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="drink" id="drink" />
+              <Label htmlFor="drink">특산주 검색</Label>
+            </div>
+          </RadioGroupStyled>
+          <SearchInput>
+            <Input placeholder={searchType === 'tag' ? '#달달한 술' : '오메기술'} />
+            <SearchIcon />
+          </SearchInput>
+          <span>찾고 싶은 게시글의 태그 또는 특산주 이름을 입력해 주세요.</span>
         </SearchContent>
       </SearchFixed>
-      <SearchWrapper>
-        <TagWrapper>
-          <div>
-            <img src={handImg} alt="추천" />
-            {Array.from({ length: 15 }, (_, idx) => (
-              <BadgeStyled variant="outline" key={idx}>
-                #달달한
-              </BadgeStyled>
+      <RecommendResultWrapper>
+        <Recommend>
+          <RecommendTitle>
+            <b>
+              이번 주{' '}
+              <span>{searchType === 'tag' ? '인기 태그 Top 15' : '인기 특산주 Top 15'}</span>
+            </b>
+            <span>09.16 ~ 09.22 기준</span>
+          </RecommendTitle>
+          <RecommendContent>
+            {suggestedData.map((item, idx) => (
+              <div key={idx}>
+                <span>{idx + 1}</span>
+                <span>
+                  {searchType === 'tag'
+                    ? (item as SuggestedTag).tagName
+                    : (item as SuggestedDrink).drinkName}
+                </span>
+              </div>
             ))}
-          </div>
-        </TagWrapper>
-        {/* <SearchResults /> */}
-      </SearchWrapper>
+          </RecommendContent>
+        </Recommend>
+        {/* 결과 입력 시 
+        <SearchResults />*/}
+      </RecommendResultWrapper>
     </SearchLayout>
   );
 };
@@ -51,12 +107,11 @@ const SearchLayout = styled.div`
   justify-content: center;
   align-items: flex-start;
   position: relative;
-  min-width: 360px;
   width: 100%;
   max-width: auto;
-  min-height: 100vh;
-  height: auto;
-  padding: 40px 20px 20px 20px;
+
+  height: 100vh;
+  padding-top: 220px;
   background-color: ${({ theme }) => theme.colors.brightGray};
 `;
 
@@ -64,9 +119,9 @@ const SearchFixed = styled.section`
   position: fixed;
   top: 0;
   width: 100vw;
-  height: auto;
+  height: 210px;
   padding: 40px 20px 0;
-  background-color: ${({ theme }) => theme.colors.brightGray};
+  background-color: ${({ theme }) => theme.colors.white};
   z-index: 10;
 `;
 
@@ -85,58 +140,115 @@ const SearchTop = styled.div`
   }
 `;
 
-const SearchContent = styled.div`
-  margin-top: 20px;
+const RadioGroupStyled = styled(RadioGroup)`
+  display: flex;
+  margin-bottom: 10px;
+
+  button {
+    border: 1px solid ${({ theme }) => theme.colors.secondary};
+  }
+
+  button[aria-checked='true'] {
+    background-color: ${({ theme }) => theme.colors.secondary};
+
+    svg {
+      color: ${({ theme }) => theme.colors.white};
+    }
+  }
+`;
+
+const SearchInput = styled.div`
+  width: 100%;
+  position: relative;
 
   input {
-    height: 45px;
-    font-size: ${({ theme }) => theme.fontSizes.base};
+    height: 40px;
+    padding-right: 45px;
     border: 1px solid ${({ theme }) => theme.colors.primary};
-    border-radius: 30px;
+    border-radius: 20px;
+    overflow: hidden;
 
-    &:focus {
+    :focus {
       box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.focusShadow};
     }
   }
 
+  svg {
+    position: absolute;
+    top: 10px;
+    right: 15px;
+    width: 20px;
+    height: 20px;
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+const SearchContent = styled.div`
+  margin-top: 30px;
+
   span {
-    color: ${({ theme }) => theme.colors.darkGray};
+    margin-bottom: 20px;
+    color: ${({ theme }) => theme.colors.gray};
     font-size: ${({ theme }) => theme.fontSizes.xsmall};
   }
 `;
 
-const SearchWrapper = styled.div`
+const RecommendResultWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  min-width: 320px;
-  width: auto;
+  width: 100%;
   height: 100%;
-  margin-top: 170px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 3px solid pink;
 `;
 
-const TagWrapper = styled.div`
-  padding: 5px;
+const Recommend = styled.section`
+  width: 100%;
+  padding: 20px;
+`;
+
+const RecommendTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+
+  b {
+    span {
+      background: linear-gradient(to top, rgba(255, 209, 140, 0.4) 45%, transparent 15%);
+    }
+  }
+
+  > span {
+    color: ${({ theme }) => theme.colors.darkGray};
+    font-size: ${({ theme }) => theme.fontSizes.xsmall};
+  }
+`;
+const RecommendContent = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
   height: auto;
 
-  > div {
+  div {
     display: flex;
-    flex-wrap: wrap;
+    justify-content: flex-start;
     align-items: center;
-  }
+    width: 50%;
+    margin-bottom: 5px;
+    padding: 5px 0;
+    font-size: ${({ theme }) => theme.fontSizes.small};
 
-  img {
-    margin: 0 5px 5px 0;
-  }
-`;
+    span:nth-of-type(1) {
+      width: 15px;
+      font-weight: bold;
+      text-align: right;
+    }
 
-const BadgeStyled = styled(Badge)`
-  padding: 6px 10px;
-  margin: 0 5px 5px 0;
-  background-color: ${({ theme }) => theme.colors.white};
-  color: ${({ theme }) => theme.colors.primary};
-  font-weight: normal;
-  border: 1px solid ${({ theme }) => theme.colors.primary};
-  font-size: ${({ theme }) => theme.fontSizes.small};
+    span:nth-of-type(2) {
+      margin-left: 8px;
+    }
+  }
 `;
 
 export default Search;
