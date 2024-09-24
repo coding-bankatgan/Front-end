395053d9fbaf4522357b27a9bd94b948c2388395
DESKTOP-9FMCPE8\User <geun1538@naver.com>
2024-09-24 18:14:55 +0900

    ✨ 공용 axios파일 추가 #113

---
+++ src/api/axios.ts
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5173/', // API 주소 설정 필요
+});
+
+/**요청 인터셉터: 모든 요청에 Access Token 자동 추가*/
+api.interceptors.request.use(
+  config => {
+    console.log(config.url);
+
+    if (config.url && (config.url.startsWith('/login') || config.url.startsWith('/signup'))) {
+      return config;
+    }
+
+    const accessToken = Cookies.get('access_token');
+
+    if (accessToken) {
+      config.headers['Access-Token'] = accessToken;
+    } else {
+      window.location.href = '/login';
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+/**응답 인터셉터: 401 에러 처리 및 토큰 갱신*/
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  async error => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = Cookies.get('refresh_token');
+
+        const refreshResponse = await axios.post(
+          '/api/members',
+          {},
+          { headers: { 'Refresh-Token': refreshToken } },
+        );
+
+        const newAccessToken = refreshResponse.headers['Access-Token'];
+        // 새 Access Token을 쿠키에 저장
+        Cookies.set('access_token', newAccessToken);
+
+        originalRequest.headers['Access-Token'] = newAccessToken;
+        return api(originalRequest);
+      } catch (error) {
+        Cookies.remove('access_token');
+        Cookies.remove('refresh_token');
+        window.location.href = '/login';
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default api;
