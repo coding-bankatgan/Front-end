6b9ab0927595daabfaa0baf65d16a725aa65714f
정유경 <loadtume5749@nhndata.com>
2024-09-28 20:04:52 +0900

    ♻️ 신고 api 리팩토링 (#143)

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -291,8 +291,8 @@ export const fetchDeclarationsWriteApi = async (
   content: string,
 ) => {
   try {
-    const response = await axios.post(
-      '/api/declarations',
+    const response = await api.post(
+      '/declarations',
       {
         link: postLink,
         type: type,
@@ -313,7 +313,7 @@ export const fetchDeclarationsWriteApi = async (
 /** 신고글 목록 조회 API */
 export const fetchDeclarationsApi = async (page: number, size: number) => {
   try {
-    const response = await axios.get(`/api/declarations?page=${page}&size=${size}`);
+    const response = await api.get(`/declarations?page=${page}&size=${size}`);
     return response.data;
   } catch (err) {
     console.error('Error fetching declarations: ', err);
@@ -323,7 +323,7 @@ export const fetchDeclarationsApi = async (page: number, size: number) => {
 /** 신고글 조회 API */
 export const fetchDeclarationsDetailApi = async (declarationId: number) => {
   try {
-    const response = await axios.get(`/api/declarations/${declarationId}`);
+    const response = await api.get(`/declarations/${declarationId}`);
     return response.data;
   } catch (err) {
     console.error('Error fetching declarations: ', err);
